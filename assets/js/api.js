e86e6df8a62236cd94bb510aebc73dd0b53f8d8a
Major2571 <caroline.tacats62@gmail.com>
2023-07-06 14:36:19 -0300

    Fetch url configuration

---
+++ assets/js/api.js
@@ -0,0 +1,27 @@
+'use strict';
+
+const api_key = "31145359a2b01ab4d2aab28237393e86";
+
+export const fetchData = function (URL, callback) {
+    fetch(`${URL}&appid=${api_key}`)
+        .then(res => res.json())
+        .then(data => callback(data));
+}
+
+export const url = {
+    currentWeather(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/weather?${lat}&${lon}&units=metric`
+    },
+    forecast(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/forecast?${lat}&${lon}&units=metric`
+    },
+    airPolution(lat, lon){
+        return `https://api.openweathermap.org/data/2.5/air_pollution?${lat}&${lon}`
+    },
+    reverseGeo(lat, lon){
+        return `https://api.openweathermap.org/geo/1.0/reverse?${lat}&${lon}&limit=5`
+    },
+    geo(query){
+        return `https://api.openweathermap.org/geo/1.0/direct?q=${query}&limit=5`
+    }
+}
